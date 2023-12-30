0fdcfa1e0045ed48b25c7444055bcce0a9628877
Dhuha Nasser <153027457+dhuhanasser2002@users.noreply.github.com>
2023-12-30 17:33:48 +0300

    Update show.blade.php

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -5,9 +5,6 @@
 @section('content')
         <div class="card-body">
             <h2 class="card-title">{{ $tag->name }}</h2>
-            @if ($tag->image)
-                <img src="{{ asset('images/' . $tag->image) }}" alt="tag Image" class="img-fluid">
-            @endif
             <div class="mt-2">
                 <a href="{{ route('tags.index') }}" class="btn btn-secondary">Back to Tags</a>
             </div>
